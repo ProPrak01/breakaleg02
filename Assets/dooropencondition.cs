a8a9ebc0a43fe4e8091110fec7cbc65bec8dfd23
Prakash <127179011+ProPrak01@users.noreply.github.com>
2023-09-12 23:16:03 +0530

    change 7

--- Assets/dooropencondition.cs
+++ Assets/dooropencondition.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class dooropencondition : MonoBehaviour
 {
+    bool i = false;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        i = FindObjectOfType<dooropencondition>().i;
+
+        if (i == true)
+        {
+
+            animator.SetTrigger("opendoor");
+
+        }
         
     }
 }
